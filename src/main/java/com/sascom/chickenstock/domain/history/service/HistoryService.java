877dd91cb20c14bff89f52f8b8dbf62e3ab8e558
namesnames <97gkswn@gmail.com>
2024-07-24 17:56:34 +0900

    [Fix] #12 - history Create 수정

--- src/main/java/com/sascom/chickenstock/domain/history/service/HistoryService.java
+++ src/main/java/com/sascom/chickenstock/domain/history/service/HistoryService.java
@@ -6,7 +6,9 @@ import com.sascom.chickenstock.domain.history.repository.HistoryRepository;
 import org.apache.catalina.Host;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 @Service
 public class HistoryService {
 
@@ -17,7 +19,8 @@ public class HistoryService {
         this.historyRepository = historyRepository;
     }
 
-    // 거래가 체결될때마다 실행되는 메서드 // 체결됐을 때 생기는 이벤트가 아래와 같은 내용을 전달해줘야함
+    // 거래가 체결될때마다 실행되는 메서드
+    @Transactional
     public void save(History request){
         History history = new History(
                 request.getAccount(),
@@ -26,5 +29,6 @@ public class HistoryService {
                 request.getPrice(),
                 request.getStatus() //이렇게 해도 되나..
         );
+        historyRepository.save(history);
     }
 }
