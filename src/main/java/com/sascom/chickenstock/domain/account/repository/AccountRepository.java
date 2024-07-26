99621052eef7cb2a14a8227c872c67401f998c40
namesnames <103047410+namesnames@users.noreply.github.com>
2024-07-26 16:10:09 +0900

    [Feat] #25 - 내 보유주식 확인 기능 개발 (#26)
    * [Feat] #25 - 내 보유주식 확인 기능 개발
    
    * [Fix] #25 - RedisService에서 주석 제거

--- src/main/java/com/sascom/chickenstock/domain/account/repository/AccountRepository.java
+++ src/main/java/com/sascom/chickenstock/domain/account/repository/AccountRepository.java
@@ -1,7 +1,11 @@
 package com.sascom.chickenstock.domain.account.repository;
 
+import com.sascom.chickenstock.domain.account.dto.response.StockInfo;
 import com.sascom.chickenstock.domain.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
 }
--- src/main/java/com/sascom/chickenstock/domain/account/service/AccountService.java
+++ src/main/java/com/sascom/chickenstock/domain/account/service/AccountService.java
@@ -1,27 +1,31 @@
 package com.sascom.chickenstock.domain.account.service;
 
-import com.sascom.chickenstock.domain.account.dto.request.AccountCreateRequest;
+import com.sascom.chickenstock.domain.account.dto.response.AccountInfoResponse;
+import com.sascom.chickenstock.domain.account.dto.response.StockInfo;
 import com.sascom.chickenstock.domain.account.entity.Account;
 import com.sascom.chickenstock.domain.account.repository.AccountRepository;
 import com.sascom.chickenstock.domain.competition.entity.Competition;
 import com.sascom.chickenstock.domain.competition.repository.CompetitionRepository;
-import com.sascom.chickenstock.domain.history.entity.History;
 import com.sascom.chickenstock.domain.member.entity.Member;
 import com.sascom.chickenstock.domain.member.repository.MemberRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.lang.model.SourceVersion;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 @RequiredArgsConstructor
 @Service
 public class AccountService {
 
-    private AccountRepository accountRepository;
-    private MemberRepository memberRepository;
-    private CompetitionRepository competitionRepository;
+    private final AccountRepository accountRepository;
+    private final MemberRepository memberRepository;
+    private final CompetitionRepository competitionRepository;
+    private final RedisService redisService;
 
     public Long createAccount(Long memberId, Long competitionId){
 
@@ -35,5 +39,36 @@ public class AccountService {
             competition
         );
         return accountRepository.save(account).getId();
+    }
+
+
+    public AccountInfoResponse getAccountInfo(Long accountId){
+        Account account = accountRepository.findById(accountId).
+                orElseThrow(EntityNotFoundException::new);
+
+        List<StockInfo> stocks = new ArrayList<>();
+        // 사용자 주식 정보 조회
+        Map<String, Map<String, String>> allStockInfo = redisService.getStockInfo(accountId);
+        for (Map.Entry<String, Map<String, String>> entry : allStockInfo.entrySet()) {
+            String key = entry.getKey();
+            Map<String, String> stockData = entry.getValue();
+            StringTokenizer st = new StringTokenizer(key,":");
+            st.nextToken();
+            st.nextToken();
+            st.nextToken();
+
+            stocks.add(new StockInfo(st.nextToken(),
+                    Integer.valueOf(stockData.get("price")),
+                    Integer.valueOf(stockData.get("volume")))
+            );
+        }
+
+        // 사용자 계좌 잔고 조회
+        AccountInfoResponse accountInfoResponse = new AccountInfoResponse(
+                account.getBalance(),
+                stocks
+        );
+
+        return accountInfoResponse;
     }
 }
