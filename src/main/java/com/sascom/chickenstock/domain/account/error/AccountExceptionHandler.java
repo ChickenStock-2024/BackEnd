2840c37429beffc99dc7a22881b08e7b97a1de47
ljh <dusk5588@naver.com>
2024-07-28 19:36:09 +0900

    [Feat] AccountExceptionHandler 구현

--- src/main/java/com/sascom/chickenstock/domain/account/error/AccountExceptionHandler.java
+++ src/main/java/com/sascom/chickenstock/domain/account/error/AccountExceptionHandler.java
@@ -1,10 +1,26 @@
 package com.sascom.chickenstock.domain.account.error;
 
+import com.sascom.chickenstock.domain.account.error.exception.AccountNotEnoughException;
+import com.sascom.chickenstock.domain.account.error.exception.AccountNotFoundException;
 import com.sascom.chickenstock.global.error.BaseExceptionHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class AccountExceptionHandler extends BaseExceptionHandler {
+    
+    @ExceptionHandler(AccountNotFoundException.class)
+    protected ResponseEntity<?> handleAccountNotFoundException(AccountNotFoundException e) {
+        log.error(e.getMessage());
+        return createErrorResponse(e.getErrorCode());
+    }
+
+    @ExceptionHandler(AccountNotEnoughException.class)
+    protected ResponseEntity<?> handleAccountNotEnoughException(AccountNotEnoughException e) {
+        log.error(e.getMessage());
+        return createErrorResponse(e.getErrorCode());
+    }
 }
