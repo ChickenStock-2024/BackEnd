6d3b6956f17d1b3a3a0e49e1d5706a5f0a9c4711
S4NGP1L <tksvlf123@gmail.com>
2024-07-29 11:01:47 +0900

    [Fix] #31 - ranking error 추가

---
+++ src/main/java/com/sascom/chickenstock/domain/ranking/error/RankingExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.sascom.chickenstock.domain.ranking.error;
+
+import com.sascom.chickenstock.domain.ranking.error.exception.RankingException;
+import com.sascom.chickenstock.global.error.BaseExceptionHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class RankingExceptionHandler extends BaseExceptionHandler {
+    protected ResponseEntity<?> handleRankingException(RankingException e) {
+        log.error(e.getMessage());
+        return createErrorResponse(e.getErrorCode());
+    }
+}
