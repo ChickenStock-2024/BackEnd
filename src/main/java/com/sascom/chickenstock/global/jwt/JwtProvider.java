8a2e760df86be7cc6b1f39a6401185d15be47888
given_dragon <yijy001@naver.com>
2024-08-02 16:30:12 +0900

    [Feat] #45 - jwt provider 구현

--- src/main/java/com/sascom/chickenstock/global/jwt/JwtProvider.java
+++ src/main/java/com/sascom/chickenstock/global/jwt/JwtProvider.java
@@ -1,21 +1,89 @@
 package com.sascom.chickenstock.global.jwt;
 
+import com.sascom.chickenstock.domain.member.entity.Member;
+import com.sascom.chickenstock.global.util.TimeUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtProvider {
+    private final JwtProperties jwtProperties;
+    private SecretKey key;
+
+    private static final String ISSUER_NAME = "chickenstock";
+    public static final String NICKNAME = "nickname";
+
+    @PostConstruct
+    private void setSecretKey() {
+        byte[] decode = Decoders.BASE64.decode(jwtProperties.getSecret());
+        key = Keys.hmacShaKeyFor(decode);
+    }
+
     public LocalDateTime getAccessTokenExpirationDate() {
-        return null;
+        LocalDateTime now = LocalDateTime.now();
+        return now.plusSeconds(jwtProperties.getAccessToken().expireDuration().toSeconds());
     }
 
     public LocalDateTime getRefreshTokenExpirationDate() {
-        return null;
+        LocalDateTime now = LocalDateTime.now();
+        return now.plusSeconds(jwtProperties.getRefreshToken().expireDuration().toSeconds());
     }
 
-    public String createToken(Authentication authentication, LocalDateTime accessTokenExpirationDate) {
-        return null;
+    @Deprecated
+    public String createToken(Member member, LocalDateTime expirationTime) {
+        Date expirationDate = TimeUtil.localDateTime2Date(expirationTime);
+
+        return Jwts.builder()
+                .issuer(ISSUER_NAME)
+                .subject(member.getId().toString())
+                .claim(NICKNAME, member.getNickname())
+                .expiration(expirationDate)
+                .signWith(key, Jwts.SIG.HS512)
+                .compact();
+    }
+
+    /**
+     * @param expirationTime > Use tokenProvider.getXXXTokenExpirationDate
+     */
+    public String createToken(Authentication authentication, LocalDateTime expirationTime) {
+        Date expirationDate = TimeUtil.localDateTime2Date(expirationTime);
+
+        return Jwts.builder()
+                .issuer(ISSUER_NAME)
+                .subject(authentication.getName())
+                .expiration(expirationDate)
+                .signWith(key, Jwts.SIG.HS512)
+                .compact();
+    }
+
+    public boolean isValidToken(String token) {
+
+        try {
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            // TODO 만료 예외처리
+        } catch (JwtException | IllegalArgumentException e) {
+            log.error(e.getMessage(), e);
+        }
+        return false;
     }
 }
