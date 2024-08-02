3a46a5a7c4a4a36bb4d095b0569e69b0942174c9
given_dragon <yijy001@naver.com>
2024-08-02 18:00:25 +0900

    [Add] #45 - 임시저장

--- src/main/java/com/sascom/chickenstock/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/sascom/chickenstock/global/filter/JwtAuthenticationFilter.java
@@ -1,10 +1,15 @@
 package com.sascom.chickenstock.global.filter;
 
+import com.sascom.chickenstock.global.jwt.JwtResolver;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,13 +19,34 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtResolver jwtResolver;
+
+    @Autowired
+    public JwtAuthenticationFilter(JwtResolver jwtResolver) {
+        this.jwtResolver = jwtResolver;
+    }
+
     // TODO Jwt 인증 필터 구현
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
-            filterChain.doFilter(request, response);
+            Cookie[] cookies = request.getCookies();
+
+            for (Cookie cookie : cookies) {
+                System.out.println(cookie.getValue());
+            }
+
+            if (cookies.length < 2) {
+                filterChain.doFilter(request, response);
+            } else {
+                Authentication authentication = jwtResolver.getAuthentication(cookies[1].getValue());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                filterChain.doFilter(request, response);
+            }
         } catch (Exception e) {
             log.info("jwt 인증 예외 발생");
+            log.info("uri: {}", request.getRequestURI());
             response.sendError(1000);
         }
     }
---
+++ src/main/java/com/sascom/chickenstock/global/oauth/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.sascom.chickenstock.global.oauth.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<?> test(HttpServletRequest request) {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        System.out.println(name);
+        return ResponseEntity.ok(name);
+    }
+}
