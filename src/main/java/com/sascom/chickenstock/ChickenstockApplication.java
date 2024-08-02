1b3cf92540a06341424e1cae64b3c5e43cef8bdd
given_dragon <yijy001@naver.com>
2024-08-02 13:51:54 +0900

    [Add] #45 - jwt 프로퍼티 설정

--- src/main/java/com/sascom/chickenstock/ChickenstockApplication.java
+++ src/main/java/com/sascom/chickenstock/ChickenstockApplication.java
@@ -2,7 +2,9 @@ package com.sascom.chickenstock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class ChickenstockApplication {
 
