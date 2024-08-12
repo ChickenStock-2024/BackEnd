43938f9168633459b2bb0faf19966704b3f86383
S4NGP1L <118046174+S4NGP1L@users.noreply.github.com>
2024-08-12 12:21:55 +0900

    [#66] Member Domain 전반적인 리팩토링 (#78)
    * [Refactor] #66 - 전반적인 코드 수정
    
    * [Fix] #66 - Member에 알림 관련 field 추가
    
    * [Feat] #66 - notication 추가
    
    * [Chore] #66 - main으로부터 pull

--- src/main/java/com/sascom/chickenstock/domain/member/controller/MemberController.java
+++ src/main/java/com/sascom/chickenstock/domain/member/controller/MemberController.java
@@ -1,21 +1,16 @@
 package com.sascom.chickenstock.domain.member.controller;
 
-import ch.qos.logback.core.util.FileUtil;
-import com.sascom.chickenstock.domain.member.dto.request.ChangeInfoRequest;
+import com.sascom.chickenstock.domain.member.dto.request.ChangePasswordRequest;
 import com.sascom.chickenstock.domain.member.dto.request.ChangeNicknameRequest;
-import com.sascom.chickenstock.domain.member.dto.response.ChangeInfoResponse;
 import com.sascom.chickenstock.domain.member.dto.response.MemberInfoResponse;
 import com.sascom.chickenstock.domain.member.dto.response.PrefixNicknameInfosResponse;
-import com.sascom.chickenstock.domain.member.entity.Member;
 import com.sascom.chickenstock.domain.member.service.MemberService;
-import com.sascom.chickenstock.global.util.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.IOException;
+
 import java.util.Map;
 
 
@@ -31,22 +26,21 @@ public class MemberController {
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<MemberInfoResponse> getMemberInfo(@PathVariable("userId") Long userId) throws IOException{
+    public ResponseEntity<MemberInfoResponse> getMemberInfo(@PathVariable("userId") Long userId) {
         MemberInfoResponse response = memberService.lookUpMemberInfo(userId);
         return ResponseEntity.ok().body(response);
     }
 
-    @PatchMapping("/{userId}")
-    public ResponseEntity<ChangeInfoResponse> patchMemberInfo(
-            @PathVariable("userId") Long userId,
-            @RequestBody ChangeInfoRequest changeInfoRequest) {
-        ChangeInfoResponse changeInfoResponse = memberService.changeMemberInfo(userId, changeInfoRequest);
-        return ResponseEntity.ok().body(changeInfoResponse);
+    @PostMapping
+    public ResponseEntity<String> patchPassword(
+            @RequestBody ChangePasswordRequest changePasswordRequest) {
+        memberService.changePassword(changePasswordRequest);
+        return ResponseEntity.ok().body("비밀번호 변경 완료");
     }
 
     @GetMapping("/search")
     public ResponseEntity<PrefixNicknameInfosResponse> getPrefixNicknameMemberInfos(
-            @RequestParam(name = "value") String prefix) throws IOException{
+            @RequestParam(name = "value") String prefix) {
         PrefixNicknameInfosResponse prefixNicknameInfosResponse = memberService.searchPrefixNicknameMemberInfos(prefix);
         return ResponseEntity.ok().body(prefixNicknameInfosResponse);
     }
@@ -57,21 +51,28 @@ public class MemberController {
         return ResponseEntity.ok().body(Map.of("nickname", changedNickname));
     }
 
-    @PostMapping(value = "/img", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Map<String, String>> postImage(@RequestParam("file") MultipartFile file) throws IOException {
-//        Member member = memberService.findById(SecurityUtil.getCurrentMemberId());
-//        memberService.setImage(member, file);
-//        return ResponseEntity.ok()
-//                .body("프로필 이미지 업로드 완료");
-        // validate file
-        memberService.setImage(file);
-        return ResponseEntity.ok().body(Map.of("msg", "프로필 이미지 업로드 완료"));
+    @PostMapping("/noti/web")
+    public ResponseEntity<Map<String, Boolean>> patchWebNotification() {
+        boolean changedState = memberService.toggleWebNotification();
+        return ResponseEntity.ok().body(Map.of("webNoti", changedState));
     }
 
-    @GetMapping(value = "/img/{userId}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> getImage(@PathVariable("userId") Long id) throws IOException {
+    @PostMapping("/noti/kakaotalk")
+    public ResponseEntity<Map<String, Boolean>> patchKakaotalkNotification() {
+        boolean changedState = memberService.toggleKakaotalkNotification();
+        return ResponseEntity.ok().body(Map.of("kakaotalkNoti", changedState));
+    }
+
+    @PostMapping(value = "/img", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> postImage(@RequestParam("file") MultipartFile file) {
+        memberService.setImage(file);
+        return ResponseEntity.ok().body("프로필 이미지 업로드 완료");
+    }
+
+    @GetMapping(value = "/img/{userId}", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
+    public ResponseEntity<byte[]> getImage(@PathVariable("userId") Long id) {
         byte[] bytes = memberService.getImage(id);
-        return new ResponseEntity<>(bytes, HttpStatus.OK);
+        return ResponseEntity.ok().body(bytes);
     }
 
     @PostMapping(value = "/img/delete")
@@ -79,4 +80,5 @@ public class MemberController {
         memberService.deleteImage();
         return ResponseEntity.ok().build();
     }
+
 }
--- src/main/java/com/sascom/chickenstock/domain/member/dto/response/ChangeInfoResponse.java
+++
@@ -1,3 +0,0 @@
-package com.sascom.chickenstock.domain.member.dto.response;
-
-public record ChangeInfoResponse(String nickname) { }
