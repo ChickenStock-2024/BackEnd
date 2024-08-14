afb720c8e5517f683b2d0768736aac3138b01f55
namesnames <97gkswn@gmail.com>
2024-08-14 13:42:38 +0900

    [Chore] #87 - main pull 후 다시 push

--- src/main/java/com/sascom/chickenstock/domain/account/service/AccountService.java
+++ src/main/java/com/sascom/chickenstock/domain/account/service/AccountService.java
@@ -187,6 +187,7 @@ public class AccountService {
         );
     }
 
+
     @Transactional
     public TradeResponse buyMarketStocks(StockOrderRequest stockOrderRequest) {
         validateStockOrderRequest(stockOrderRequest);
