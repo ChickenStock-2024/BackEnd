0ddae7e7747994ed5c7ca2609e399bb54daf51e8
given_dragon <yijy001@naver.com>
2024-08-14 12:32:51 +0900

    [Add] #99 - Competition 생성 에러코드 추가

--- src/main/java/com/sascom/chickenstock/domain/competition/error/code/CompetitionErrorCode.java
+++ src/main/java/com/sascom/chickenstock/domain/competition/error/code/CompetitionErrorCode.java
@@ -7,11 +7,15 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CompetitionErrorCode implements ChickenStockErrorCode {
     NOT_FOUND(HttpStatus.NOT_FOUND, "001", "존재하지 않는 대회입니다."),
-    INVALID_VALUE(HttpStatus.BAD_REQUEST, "002", "올바르지 않은 값입니다.");
-
+    INVALID_VALUE(HttpStatus.BAD_REQUEST, "002", "올바르지 않은 값입니다."),
+    INVALID_DATE(HttpStatus.BAD_REQUEST, "003", "올바르지 않은 날짜입니다."),
+    INVALID_DATE_TYPE(HttpStatus.BAD_REQUEST, "004", "잘못된 날짜 형식입니다."),
+    INVALID_DURATION(HttpStatus.BAD_REQUEST, "005", "올바르지 않은 기간입니다."),
+    CONFLICT(HttpStatus.BAD_REQUEST, "006", "기존 대회 기간과 중복됩니다."),
+    INVALID_KEY(HttpStatus.UNAUTHORIZED, "007", "권한이 없습니다.");
     CompetitionErrorCode(HttpStatus status, String code, String message) {
         this.status = status;
-        this.code = "ACCOUNT"+code;
+        this.code = "COMPETITION"+code;
         this.message = message;
     }
 
