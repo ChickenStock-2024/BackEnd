69b6beb5effcef0b476e3a5400211122301a0bf1
given-dragon <yijy001@gmail.com>
2024-08-16 01:10:27 +0900

    [Fix] #74 - jwt auth filter 주석해제

--- src/main/java/com/sascom/chickenstock/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/sascom/chickenstock/global/filter/JwtAuthenticationFilter.java
@@ -2,12 +2,10 @@ package com.sascom.chickenstock.global.filter;
 
 import com.sascom.chickenstock.domain.account.service.RedisService;
 import com.sascom.chickenstock.global.error.code.AuthErrorCode;
-import com.sascom.chickenstock.global.error.code.ChickenStockErrorCode;
 import com.sascom.chickenstock.global.error.exception.AccessTokenExpireException;
 import com.sascom.chickenstock.global.error.exception.ChickenStockException;
 import com.sascom.chickenstock.global.error.exception.TokenNotFoundException;
 import com.sascom.chickenstock.global.jwt.JwtProperties;
-import com.sascom.chickenstock.global.jwt.JwtProvider;
 import com.sascom.chickenstock.global.jwt.JwtResolver;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -19,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
@@ -32,73 +29,51 @@ import java.util.Optional;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtResolver jwtResolver;
-    private final JwtProvider jwtProvider;
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String BEARER_PREFIX = "Bearer ";
     private final JwtProperties jwtProperties;
     private final RedisService redisService;
 
     @Autowired
-    public JwtAuthenticationFilter(JwtResolver jwtResolver, JwtProvider jwtProvider, JwtProperties jwtProperties, RedisService redisService) {
+    public JwtAuthenticationFilter(JwtResolver jwtResolver, JwtProperties jwtProperties, RedisService redisService) {
         this.jwtResolver = jwtResolver;
-        this.jwtProvider = jwtProvider;
         this.jwtProperties = jwtProperties;
         this.redisService = redisService;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-//        try {
-//            Cookie accesTokenCookie = WebUtils.getCookie(request, jwtProperties.accessToken().cookieName());
-//            if (accesTokenCookie == null) {
-//                ChickenStockException e = TokenNotFoundException.of(AuthErrorCode.TOKEN_NOT_FOUND);
-//                request.setAttribute("exception", e);
-//                throw e;
-//            }
-//
-//            String accessToken = accesTokenCookie.getValue();
-//
-//            if (!StringUtils.hasText(accessToken)) {
-//                ChickenStockException e = TokenNotFoundException.of(AuthErrorCode.TOKEN_NOT_FOUND);
-//                request.setAttribute("exception", e);
-//                throw e;
-//            }
-//
-//            if (!jwtResolver.isValidToken(accessToken)) {
-//                ChickenStockException e = AccessTokenExpireException.of(AuthErrorCode.ACCESS_TOKEN_EXPIRED);
-//                request.setAttribute("exception", e);
-//                throw e;
-//            }
-//
-//            // 블랙리스트에 올라간 Access Token 검증
-//            Optional<String> blacklistToken = redisService.getValues(accessToken);
-//            if(blacklistToken.isEmpty()) {
-//                Authentication authentication = jwtResolver.getAuthentication(accessToken);
-//                SecurityContextHolder.getContext().setAuthentication(authentication);
-//            }
-//
-//        } catch (TokenNotFoundException | AccessTokenExpireException e) {
-//            log.error("exception class:{}, message: {}", e.getClass(), e.getMessage());
-//        }
+        try {
+            Cookie accesTokenCookie = WebUtils.getCookie(request, jwtProperties.accessToken().cookieName());
+            if (accesTokenCookie == null) {
+                ChickenStockException e = TokenNotFoundException.of(AuthErrorCode.TOKEN_NOT_FOUND);
+                request.setAttribute("exception", e);
+                throw e;
+            }
 
-        Authentication testAuthentication = jwtResolver.getTestAuthentication();
-        SecurityContextHolder.getContext().setAuthentication(testAuthentication);
+            String accessToken = accesTokenCookie.getValue();
 
+            if (!StringUtils.hasText(accessToken)) {
+                ChickenStockException e = TokenNotFoundException.of(AuthErrorCode.TOKEN_NOT_FOUND);
+                request.setAttribute("exception", e);
+                throw e;
+            }
 
+            if (!jwtResolver.isValidToken(accessToken)) {
+                ChickenStockException e = AccessTokenExpireException.of(AuthErrorCode.ACCESS_TOKEN_EXPIRED);
+                request.setAttribute("exception", e);
+                throw e;
+            }
 
-//        Authentication testAuthentication = jwtResolver.getTestAuthentication();
-//        SecurityContextHolder.getContext().setAuthentication(testAuthentication);
+            // 블랙리스트에 올라간 Access Token 검증
+            Optional<String> blacklistToken = redisService.getValues(accessToken);
+            if(blacklistToken.isEmpty()) {
+                Authentication authentication = jwtResolver.getAuthentication(accessToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
 
-
+        } catch (TokenNotFoundException | AccessTokenExpireException e) {
+            log.error("exception class:{}, message: {}", e.getClass(), e.getMessage());
+        }
 
         filterChain.doFilter(request, response);
-    }
-
-    private String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
-            return bearerToken.substring(7);
-        }
-        return null;
     }
 }
