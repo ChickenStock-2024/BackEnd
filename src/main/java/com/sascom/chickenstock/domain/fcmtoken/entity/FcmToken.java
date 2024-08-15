73974757aea8bf6285f255d27677474f2e3dc0d9
given-dragon <yijy001@gmail.com>
2024-08-16 00:59:54 +0900

    [Fix] #74 - fcm token service 생성

--- src/main/java/com/sascom/chickenstock/domain/fcmtoken/entity/FcmToken.java
+++ src/main/java/com/sascom/chickenstock/domain/fcmtoken/entity/FcmToken.java
@@ -5,15 +5,32 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.util.StringUtils;
 
 @Entity
-@Setter
 @Getter
+@NoArgsConstructor
 public class FcmToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long memberId;
     private String token;
+
+    public FcmToken(Long memberId, String token) {
+        this.memberId = memberId;
+        this.token = token;
+    }
+
+    public void updateToken(String token) {
+        if (!StringUtils.hasText(token)) {
+            throw new IllegalArgumentException("fcm token이 없습니다.");
+        }
+        if (token.equals(this.token)) {
+            return;
+        }
+        this.token = token;
+    }
 }
---
+++ src/main/java/com/sascom/chickenstock/domain/fcmtoken/service/FcmTokenService.java
@@ -0,0 +1,45 @@
+package com.sascom.chickenstock.domain.fcmtoken.service;
+
+import com.sascom.chickenstock.domain.fcmtoken.dto.response.FcmTokenResponse;
+import com.sascom.chickenstock.domain.fcmtoken.entity.FcmToken;
+import com.sascom.chickenstock.domain.fcmtoken.repository.FcmTokenRepository;
+import com.sascom.chickenstock.global.util.SecurityUtil;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class FcmTokenService {
+
+    private final FcmTokenRepository fcmTokenRepository;
+
+    public FcmTokenResponse storeFcmToken(Long memberId, String newToken) {
+
+        Long loginMemberId = SecurityUtil.getCurrentMemberId();
+        if (!loginMemberId.equals(memberId)) {
+            throw new IllegalArgumentException("로그인 멤버가 일치하지 않습니다.");
+        }
+
+        Optional<FcmToken> candiFcmToken = fcmTokenRepository.findByMemberId(memberId);
+        candiFcmToken.ifPresentOrElse(
+                (oldToken) -> oldToken.updateToken(newToken),
+                () -> saveFcmToken(memberId, newToken)
+        );
+
+        return FcmTokenResponse.builder()
+                .message("Success to Save FcmToken")
+                .fcmToken(newToken)
+                .memberId(memberId)
+                .build();
+    }
+
+    public FcmToken saveFcmToken(Long memberId, String fcmToken) {
+        FcmToken token = new FcmToken(memberId, fcmToken);
+        return fcmTokenRepository.save(token);
+    }
+}
