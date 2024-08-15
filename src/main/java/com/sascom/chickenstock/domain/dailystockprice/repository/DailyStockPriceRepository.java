a40c73894adcef2f3af40dafea80a95974533902
S4NGP1L <118046174+S4NGP1L@users.noreply.github.com>
2024-08-15 19:11:19 +0900

    [Fix] #124 - 서버 가동시 시장가가 비어있는 문제 수정 (#125)

--- src/main/java/com/sascom/chickenstock/domain/dailystockprice/repository/DailyStockPriceRepository.java
+++ src/main/java/com/sascom/chickenstock/domain/dailystockprice/repository/DailyStockPriceRepository.java
@@ -2,6 +2,8 @@ package com.sascom.chickenstock.domain.dailystockprice.repository;
 
 import com.sascom.chickenstock.domain.dailystockprice.entity.DailyStockPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,4 +12,6 @@ import java.util.Optional;
 public interface DailyStockPriceRepository extends JpaRepository<DailyStockPrice, Long> {
     List<DailyStockPrice> findDailyStockPriceByCompanyId(Long companyId);
     Optional<DailyStockPrice> findDailyStockPriceByDateTimeAndCompanyId(LocalDate localDate, Long companyId);
+
+    Optional<DailyStockPrice> findFirstByCompanyIdOrderByDateTimeDesc(Long companyId);
 }
