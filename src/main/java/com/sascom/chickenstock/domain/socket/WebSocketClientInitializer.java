3904de6864a59b59f27322b3a74cbed4692cf8fd
S4NGP1L <tksvlf123@gmail.com>
2024-08-16 09:52:08 +0900

    [Chore] #130 - console 출력 제거

--- src/main/java/com/sascom/chickenstock/domain/socket/WebSocketClientInitializer.java
+++ src/main/java/com/sascom/chickenstock/domain/socket/WebSocketClientInitializer.java
@@ -30,8 +30,7 @@ public class WebSocketClientInitializer implements InitializingBean {
     public void afterPropertiesSet() {
         try {
             StompSession session = stompClient.connectAsync(url, stompSessionHandler).get();
-
-            System.out.println("WebSocket connection initialized");
+//            System.out.println("WebSocket connection initialized");
         } catch (Exception e) {
             e.printStackTrace();
         }
